a6899e6c4f68f6e9bcff5bdbd65c961c65076a84
Li, Jack <chl218@eng.ucsd.edu>
2021-01-19 21:21:58 -0800

    triangle

--- 00-turtle/myturtle.py
+++ 00-turtle/myturtle.py
@@ -9,11 +9,17 @@ def square(side_len=100):
         forward(side_len)
         right(90)
 
+def triangle(side_len = 100):
+   for i in range(3):
+      forward(side_len)
+      right(120)
+
 def circles_of_squares():
     for i in range(60):
         square()
         right(5)
 
 
+triangle()
 
 input()
